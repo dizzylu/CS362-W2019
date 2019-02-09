c0c64e7f4e75fbf7853f5caf693437fe2c3c071d
Luis Malicay <malicayl@oregonstate.edu>
2019-02-09 13:12:35 -0700

    Cardtest1 tests if the smithy card added 3 to the player's hand.

---
+++ projects/malicayl/dominion/cardtest1.c
@@ -0,0 +1,54 @@
+/*
+ * CS362 - Assignment #3
+ * Due: 2/10/19
+ * Description: Card test for Smithy
+ */
+#include "dominion_helpers.h"
+#include "rngs.h"
+#include <stdio.h>
+#include <string.h>
+
+// Tester defined assert
+int assertTrue(int test, int expected)
+{
+    if(test != expected)
+        return 0;
+    else
+        return 1;
+}
+
+int main(int argc, char const *argv[])
+{
+    printf("\n\nCard Test: Smithy\n----------------------------\n\n");
+
+    // Initialize a game to run tests on
+    // Source: CS362 card test sample file
+    int handpos = 0, choice1 = 0, choice2 = 0, choice3 = 0, bonus = 0;
+    int numPlayers = 2;
+    int k[10] = {adventurer, embargo, village, minion, mine,
+                 cutpurse, sea_hag, tribute, smithy, council_room};
+    int seed = 1000;
+
+    // Use two states to copy and compare
+    struct gameState G, testG;
+    initializeGame(numPlayers, k, seed, &G);
+    memcpy(&testG, &G, sizeof(struct gameState));
+
+    // Display current count of both states
+    printf("Player 1 Hand Count\nOriginal State: %i\tTest State: %i\n", G.handCount[0], testG.handCount[0]);
+
+    // Test that current player drew 3 cards
+    printf("\nPlay Smithy card\n");
+    cardEffect(smithy, choice1, choice2, choice3, &testG, handpos, &bonus);
+    int oldCount = G.handCount[0];
+    int newCount = testG.handCount[0];
+    printf("Original State: %i\tTest State: %i\n", oldCount, newCount);
+
+    int result = assertTrue(newCount, oldCount+3);
+    if(result)
+        printf("\nTest Passed: 3 cards added to deck\n\n\n");
+    else if(!result)
+        printf("\nTest Failed: Incorrect amount of cards added to deck\n\n\n");
+
+    return 0;
+}
