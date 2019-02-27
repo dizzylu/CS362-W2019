fa4e7e86f8b2ee6fff7871f35e37d6724c4ea80f
Luis Malicay <malicayl@oregonstate.edu>
2019-02-26 19:36:04 -0700

    File for adventurer random test card created.

---
+++ projects/malicayl/dominion/randomtestadventurer.c
@@ -0,0 +1,28 @@
+/**
+ * CS362 - Assignment #4
+ * Due: 2/27/19
+ * Description: Random test generator for Adventurer card
+ */
+
+#include "dominion_helpers.h"
+#include "rngs.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+const int numTests = 100;
+
+// Tester defined assert
+int assertTrue(int a, int b)
+{
+    if(a != b)
+        return 0;
+    else
+        return 1;
+}
+
+int main()
+{
+    return 0;
+}
