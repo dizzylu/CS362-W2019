7f725f89cca7ecab8b7d94046c978f1f1275768b
Luis Malicay <malicayl@oregonstate.edu>
2019-02-27 17:18:51 -0700

    Refactored test card 2.

--- projects/malicayl/dominion/randomtestcard2.c
+++ projects/malicayl/dominion/randomtestcard2.c
@@ -11,9 +11,8 @@
 #include <string.h>
 #include <time.h>
 
-const int numTests = 100;
+#define TESTCARD smithy
 
-// Tester defined assert
 int assertTrue(int a, int b)
 {
     if(a != b)
@@ -24,59 +23,69 @@ int assertTrue(int a, int b)
 
 int main()
 {
-    srand(time(NULL));
     printf("\n\nRandom Testing: Smithy Card\n------------------------------\n\n");
 
     // Set game parameters
-    // Source: CS362 Card Test sample
+    // Source: CS362 card test sample
+    srand(time(NULL));
+    const int numTests = 100;
     struct gameState Game, testG;
     int k[10] = {adventurer, embargo, village, minion, mine,
                  cutpurse, sea_hag, tribute, smithy, council_room};
-    int seed = 1000;
+    int seed = 1000, passed = 0, failed = 0, failedInit = 0, res = 0,
+        maxPlay = 4, minPlay = 2;
 
     // Run tests
-    int passed = 0, failed = 0, failedInit = 0, res;
     for(int i = 0; i < numTests; ++i)
     {
-        // Set up a game and copy state to make comparisons
         printf("Test #%i\n", i+1);
-        int numPlayers = rand() % MAX_PLAYERS + 2;
+
+        // Randomize number of players and player turn
+        int numPlayers = rand() % (maxPlay - minPlay + 1) + minPlay,
+            randTurn = rand() % numPlayers;
+
+        // Setup a game and copy state to make comparisons
         int setup = initializeGame(numPlayers, k, seed, &Game);
         if(setup == -1)
         {
             printf("*Game Initialization Failed*\n");
             failedInit++;
         }
+        Game.whoseTurn = randTurn;
         memcpy(&testG, &Game, sizeof(struct gameState));
 
         // Randomize choices and play card
-        int choice1 = rand() % 3 + 1, choice2 = rand() % 3 + 1, choice3 = rand() % 3 + 1, bonus = 0;
-        cardEffect(smithy, choice1, choice2, choice3, &testG, 0, &bonus);
+        int choice1 = rand(), choice2 = rand(), choice3 = rand(), bonus = rand();
+        cardEffect(TESTCARD, choice1, choice2, choice3, &testG, 0, &bonus);
 
-        // Make comparisons
-        int playerTurn = whoseTurn(&Game),
-            origCount = Game.handCount[playerTurn],
-            testCount = testG.handCount[playerTurn];
+        // Mimic smithy card
+        for(int j = 0; j < 3; ++j)
+            drawCard(randTurn, &Game);
+        discardCard(0, randTurn, &Game, 0);
 
-        printf("Original Game - Hand Count: %i\n", origCount);
-        printf("Test Game - Hand Count: %i\n", testCount);
-        res = assertTrue(origCount, testCount-3);
+        // Compare with original state
+        int origHand = Game.handCount[randTurn], testHand = testG.handCount[randTurn];
+
+        // 3 cards added to hand
+        printf("Original Game - Hand Count: %i\n", origHand);
+        printf("Test Game - Hand Count: %i\n", testHand);
+        res = assertTrue(origHand, testHand);
         if(!res)
         {
-            printf("Test Failed: +3 cards not added\n\n");
+            printf("Test Failed: +1 card not added\n\n");
             failed++;
         }
         else if(res)
         {
-            printf("Test Passed: +3 cards added\n\n");
+            printf("Test Passed: +1 card added\n\n");
             passed++;
         }
 
-        // Clear game states
         memset(&Game, 0, sizeof(Game));
         memset(&testG, 0, sizeof(testG));
     }
 
+    // Display results
     printf("\n------------------------------\n");
     printf("Passing Tests: %i\nFailing Tests: %i\nGame Setup Failures: %i\n\n", passed, failed, failedInit);
 
